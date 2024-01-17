ad323b8aa2f63599e847e4d4f2b9475a0e4adcac
sameeul <sameeul@gmail.com>
2024-01-17 12:31:50 -0500

    Replace labshare to PolusAI

--- build-docker.sh
+++ build-docker.sh
@@ -3,6 +3,6 @@
 version=$(<VERSION)
 
 # Python builds
-docker build . -f ./docker/base/Dockerfile -t labshare/polus-bfio-util:"${version}"
-docker build . -f ./docker/tensorflow/Dockerfile -t labshare/polus-bfio-util:"${version}"-tensorflow
-docker build . -f ./docker/imagej/Dockerfile -t labshare/polus-bfio-util:"${version}"-imagej
+docker build . -f ./docker/base/Dockerfile -t polusai/bfio:"${version}"
+docker build . -f ./docker/tensorflow/Dockerfile -t polusai/bfio:"${version}"-tensorflow
+docker build . -f ./docker/imagej/Dockerfile -t polusai/bfio:"${version}"-imagej
