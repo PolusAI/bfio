2a2bc578bb2cfb7e9b57273f602d9387244c68de
sameeul <sameeul@gmail.com>
2024-02-12 13:13:47 -0500

    update bio-formats version

--- src/bfio/bfio.py
+++ src/bfio/bfio.py
@@ -27,7 +27,7 @@ try:
         """
 
         global JAR_VERSION
-        scyjava.config.endpoints.append("ome:formats-gpl:7.1.0")
+        scyjava.config.endpoints.append("ome:formats-gpl:7.2.0")
         scyjava.start_jvm()
         import loci
 
